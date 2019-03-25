5bda70172d5c21a3a69f58e77f9758c1789acaa5
Jack <westernmonster@gmail.com>
2019-03-26 06:08:25 +0800

    Email Templates

--- infrastructure/email/email.go
+++ infrastructure/email/email.go
@@ -1,6 +1,8 @@
 package email
 
 import (
+	"git.flywk.com/flywiki/api/infrastructure/email/tmpl"
+	"git.flywk.com/flywiki/api/infrastructure/email/tmpl/layouts"
 	"github.com/denverdino/aliyungo/dm"
 )
 
@@ -18,6 +20,16 @@ func (p *EmailClient) SendRegisterEmail(email string, valcode string) (err error
 		Subject:        "飞行百科注册验证码",
 	}
 
+	body := &tmpl.RegisterValcodeBody{
+		Head: &layouts.EmailPageHead{
+			Title:     "飞行百科注册验证码",
+			BodyClass: "",
+		},
+		Valcode: valcode,
+	}
+
+	args.HtmlBody = body.EmailHTML()
+
 	err = p.Client.SendSingleMail(args)
 	return
 }
@@ -29,6 +41,16 @@ func (p *EmailClient) SendResetPasswordValcode(email string, valcode string) (er
 		FromAlias:      "飞行百科",
 		Subject:        "飞行百科重置密码",
 	}
+
+	body := &tmpl.RegisterValcodeBody{
+		Head: &layouts.EmailPageHead{
+			Title:     "飞行百科安全验证码",
+			BodyClass: "",
+		},
+		Valcode: valcode,
+	}
+
+	args.HtmlBody = body.EmailHTML()
 
 	err = p.Client.SendSingleMail(args)
 	return
