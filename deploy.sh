263e9274f589acbcc9b4b4b9bcc282f982067ed5
Jack <westernmonster@gmail.com>
2019-06-04 09:36:28 +0800

    refactor

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,4 @@ packr
 swag init
 gox -osarch="linux/amd64" -output="api"
 rsync -avz --delete ./api root@118.31.11.232:/home/www/api/
-rsync -avz --delete ./migrations root@118.31.11.232:/home/www/api/
-rsync -avz --delete ./docs root@118.31.11.232:/home/www/api/
-rsync -avz --delete ./config.json root@118.31.11.232:/home/www/api/
-rsync -avz --delete ./dbconfig.yml root@118.31.11.232:/home/www/api/
+rsync -avz --delete    ./docs/swagger/swagger.json root@118.31.11.232:/home/www/apidoc/build/
