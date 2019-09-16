6894ad09c64a70972564cbf147d9349179411ab4
Jack <westernmonster@gmail.com>
2019-09-17 03:09:45 +0800

    test ci

--- build/print-workspace-status.sh
+++ build/print-workspace-status.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 # Copyright 2017 The Kubernetes Authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,39 +13,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# This command is used by bazel as the workspace_status_command
-# to implement build stamping with git information.
-
 set -o errexit
 set -o nounset
 set -o pipefail
 
-export KRATOS_ROOT=$(dirname "${BASH_SOURCE}")/..
-
-source "${KRATOS_ROOT}/build/lib/version.sh"
-kratos::version::get_version_vars
-
-# Prefix with STABLE_ so that these values are saved to stable-status.txt
-# instead of volatile-status.txt.
-# Stamped rules will be retriggered by changes to stable-status.txt, but not by
-# changes to volatile-status.txt.
-# IMPORTANT: the camelCase vars should match the lists in hack/lib/version.sh
-# and pkg/version/def.bzl.
+git_commit="$(git describe --tags --always --dirty)"
+build_date="$(date -u '+%Y%m%d')"
+docker_tag="v${build_date}-${git_commit}"
+# TODO(fejta): retire STABLE_PROW_REPO
 cat <<EOF
-STABLE_BUILD_GIT_COMMIT ${KRATOS_GIT_COMMIT-}
-STABLE_BUILD_SCM_STATUS ${KRATOS_GIT_TREE_STATE-}
-STABLE_BUILD_SCM_REVISION ${KRATOS_GIT_VERSION-}
-STABLE_BUILD_MAJOR_VERSION ${KRATOS_GIT_MAJOR-}
-STABLE_BUILD_MINOR_VERSION ${KRATOS_GIT_MINOR-}
-STABLE_BUILD_TIME ${KRATOS_BUILD_TIME-}
-STABLE_DOCKER_TAG ${KRATOS_GIT_VERSION/+/_}
-gitCommit ${KRATOS_GIT_COMMIT-}
-gitTreeState ${KRATOS_GIT_TREE_STATE-}
-gitVersion ${KRATOS_GIT_VERSION-}
-gitMajor ${KRATOS_GIT_MAJOR-}
-gitMinor ${KRATOS_GIT_MINOR-}
-buildDate $(date \
-  ${SOURCE_DATE_EPOCH:+"--date=@${SOURCE_DATE_EPOCH}"} \
- -u +'%Y-%m-%dT%H:%M:%SZ')
+STABLE_BUILD_GIT_COMMIT ${git_commit}
+DOCKER_TAG ${docker_tag}
 EOF
-
