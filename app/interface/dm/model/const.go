cf208e771c85c8072bba2fff1dcfcbc32fd1d297
Jack <westernmonster@gmail.com>
2019-10-18 13:38:27 +0800

    dm

--- app/interface/dm/model/const.go
+++ app/interface/dm/model/const.go
@@ -10,3 +10,12 @@ const (
 	TargetTypeTopicInviteRequest = "invite"
 	TargetTypeTopicFollowRequest = "follow"
 )
+
+// topic      话题
+// discussion 讨论
+// revise     补充
+// article    文章
+// member     成员
+// comment    评论
+// invite     邀请请求
+// follow     加入请求
