6fee5f9623ab0f52e16710cf05b81876402dc962
Jack <westernmonster@gmail.com>
2019-10-18 13:38:12 +0800

    dm

--- app/interface/account/http/member.go
+++ app/interface/account/http/member.go
@@ -80,6 +80,23 @@ func memberInfo(c *mars.Context) {
 	c.JSON(srv.GetMemberInfo(c, id))
 }
 
+// @Summary 获取用户认证信息
+// @Description 获取用户认证信息
+// @Tags account
+// @Accept json
+// @Produce json
+// @Param Authorization header string true "Bearer"
+// @Param Source header int true "Source 来源，1:Web, 2:iOS; 3:Android" Enums(1, 2, 3)
+// @Param Locale header string true "语言" Enums(zh-CN, en-US)
+// @Param id query string true "ID"
+// @Success 200 {object} model.MemberCertInfo "认证资料"
+// @Failure 400 "验证请求失败"
+// @Failure 401 "登录验证失败"
+// @Failure 500 "服务器端错误"
+// @Router /account/cert [get]
+func memberCert(c *mars.Context) {
+}
+
 // @Summary 获取用户动态
 // @Description 获取用户动态
 // @Tags account
---
+++ app/interface/account/model/resp_cert.go
@@ -0,0 +1,48 @@
+package model
+
+type MemberCertInfo struct {
+	// 申请人
+	Member *MemberInfo `json:"member"`
+
+	// 身份认证信息
+	IDCert *IDCertificationInfo `json:"id_cert"`
+
+	// 工作认证信息
+	WorkCert *WorkCertificationInfo `json:"work_cert"`
+}
+
+type IDCertificationInfo struct {
+	// 账户ID
+	ID int64 `json:"id,string" swaggertype:"string"`
+	// 姓名
+	Name *string `json:"name,omitempty"`
+
+	// 证件号
+	IdentificationNumber *string `json:"identification_number,omitempty"`
+	// 证件类型, identityCard代表身份证
+	IDCardType *string `json:"id_card_type,omitempty"`
+
+	// Status 状态：-1 未认证, 0 认证中,  1 认证成功, 2 认证失败
+	Status int `json:"status"`
+
+	UpdatedAt int64 `json:"updated_at"`
+}
+
+type WorkCertificationInfo struct {
+	// 账户ID
+	ID int64 `json:"id,string" swaggertype:"string"`
+
+	// 公司
+	Company *string `json:"company,omitempty"`
+
+	// 部门
+	Department *string `json:"department,omitempty"`
+
+	// 职位
+	Position *string `json:"position,omitempty"`
+
+	// Status 状态：-1 未认证, 0 认证中,  1 认证成功, 2 认证失败
+	Status int `json:"status"`
+
+	UpdatedAt int64 `json:"updated_at"`
+}
