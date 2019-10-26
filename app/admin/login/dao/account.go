0dda2efa9a0fabee726cf477e2783d9dd2183604
Jack <westernmonster@gmail.com>
2019-10-26 10:25:09 +0800

    refactor account

--- app/admin/login/dao/account.go
+++ app/admin/login/dao/account.go
@@ -68,31 +68,13 @@ func (p *Dao) SetPassword(c context.Context, node sqalx.Node, password, salt str
 }
 
 func (p *Dao) UpdateAccount(c context.Context, node sqalx.Node, item *model.Account) (err error) {
-	if _,
-		err = node.ExecContext(c,
-		_updateAccountSQL,
-		item.Mobile,
-		item.UserName,
-		item.Email,
-		item.Password,
-		item.Role,
-		item.Salt,
-		item.Gender,
-		item.BirthYear,
-		item.BirthMonth,
-		item.BirthDay,
-		item.Location,
-		item.Introduction,
-		item.Avatar,
-		item.Source,
-		item.IP,
-		item.IDCert,
-		item.WorkCert,
-		item.IsOrg,
-		item.IsVIP,
-		item.UpdatedAt,
-		item.ID); err != nil {
-		log.For(c).Error(fmt.Sprintf("dao.UpdateAccount error(%+v), item(%+v)", err, item))
+	sqlUpdate := "UPDATE accounts SET mobile=?,user_name=?,email=?,password=?,role=?,salt=?,gender=?,birth_year=?,birth_month=?,birth_day=?,location=?,introduction=?,avatar=?,source=?,ip=?,id_cert=?,work_cert=?,is_org=?,is_vip=?,updated_at=?,prefix=? WHERE id=?"
+
+	_, err = node.ExecContext(c, sqlUpdate, item.Mobile, item.UserName, item.Email, item.Password, item.Role, item.Salt, item.Gender, item.BirthYear, item.BirthMonth, item.BirthDay, item.Location, item.Introduction, item.Avatar, item.Source, item.IP, item.IDCert, item.WorkCert, item.IsOrg, item.IsVip, item.UpdatedAt, item.Prefix, item.ID)
+	if err != nil {
+		log.For(c).Error(fmt.Sprintf("dao.UpdateAccounts err(%+v), item(%+v)", err, item))
+		return
 	}
+
 	return
 }
