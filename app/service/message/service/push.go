08279e1d33c417430c6a6fe06dec9750c33788bd
Jack <westernmonster@gmail.com>
2019-10-28 14:09:35 +0800

    service-message

--- app/service/message/service/push.go
+++ app/service/message/service/push.go
@@ -10,7 +10,7 @@ import (
 func (p *Service) pushSingleUser(c context.Context, aid int64, message string) (msgID string, err error) {
 	payload := &jpush.Payload{
 		Platform: jpush.NewPlatform().All(),
-		Audience: jpush.NewAudience().SetRegistrationId(fmt.Sprintf("%d", aid)),
+		Audience: jpush.NewAudience().SetAlias(fmt.Sprintf("%d", aid)),
 		Notification: &jpush.Notification{
 			Alert: message,
 		},
