e42a75a25df1ef19dd28d12292e92141cc82ce1b
Jack <westernmonster@gmail.com>
2019-11-01 22:38:08 +0800

    refactor topic

--- app/interface/account/service/member.go
+++ app/interface/account/service/member.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"net/url"
 	"strconv"
 
@@ -201,6 +202,8 @@ func (p *Service) GetMemberInfo(c context.Context, targetID int64) (resp *model.
 		err = ecode.AcquireAccountIDFailed
 		return
 	}
+
+	fmt.Printf("aid(%d) targetID(%d)", aid, targetID)
 
 	var f *account.MemberInfoReply
 	if f, err = p.d.GetMemberInfo(c, targetID); err != nil {
