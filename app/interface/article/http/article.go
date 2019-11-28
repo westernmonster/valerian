21522ab7abcc43d588b169a9a0f343bbc67688c5
Jack <westernmonster@gmail.com>
2019-11-28 12:42:58 +0800

    discuss

--- app/interface/article/http/article.go
+++ app/interface/article/http/article.go
@@ -133,8 +133,8 @@ func getArticle(c *mars.Context) {
 // @Param Source header int true "Source 来源，1:Web, 2:iOS; 3:Android" Enums(1, 2, 3)
 // @Param Locale header string true "语言" Enums(zh-CN, en-US)
 // @Param query query string true "查询条件"
-// @Param limit query integer false "每页大小"
-// @Param offset query integer false "offset"
+// @Param ps query integer false "每页大小"
+// @Param pn query integer false "页码 1开始"
 // @Success 200 {object}  app.interface.article.model.ArticleListResp "文章"
 // @Failure 400 "验证请求失败"
 // @Failure 401 "登录验证失败"
