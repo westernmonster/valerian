2a58b99c88186dca60d7954bfbf825ec6f5cfc99
Jack <westernmonster@gmail.com>
2019-11-28 16:21:37 +0800

    file

--- app/interface/article/http/article_file.go
+++ app/interface/article/http/article_file.go
@@ -73,7 +73,7 @@ func articleFiles(c *mars.Context) {
 // @Param Source header int true "Source 来源，1:Web, 2:iOS; 3:Android" Enums(1, 2, 3)
 // @Param Locale header string true "语言" Enums(zh-CN, en-US)
 // @Param file_id query string true "文件ID"
-// @Success 200 {array}  app.interface.article.model.ArticleFileResp "文件列表"
+// @Success 200 {object}  app.interface.article.model.ArticleFileResp "文件列表"
 // @Failure 400 "请求验证失败"
 // @Failure 401 "登录验证失败"
 // @Failure 500 "服务器端错误"
