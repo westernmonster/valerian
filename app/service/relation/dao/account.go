fd4db841aa69b60d8dd7c7f104a940cbb11e62b9
Jack <westernmonster@gmail.com>
2019-12-03 23:07:04 +0800

    fix bugs of accounts

--- app/service/relation/dao/account.go
+++ app/service/relation/dao/account.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	_getAccountByIDSQL = "SELECT a.* FROM accounts a WHERE a.deleted=0 AND a.id=?"
+	_getAccountByIDSQL = "SELECT a.id,a.mobile,a.user_name,a.email,a.password,a.role,a.salt,a.gender,a.birth_year,a.birth_month,a.birth_day,a.location,a.introduction,a.avatar,a.source,a.ip,a.id_cert,a.work_cert,a.is_org,a.is_vip,a.deleted,a.created_at,a.updated_at,a.prefix  FROM accounts a WHERE a.deleted=0 AND a.id=?"
 )
 
 func (p *Dao) GetAccountByID(c context.Context, node sqalx.Node, id int64) (item *model.Account, err error) {
