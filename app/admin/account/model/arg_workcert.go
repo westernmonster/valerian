b6ccfb2f8554e47c5cf2d3f67f001cf770a096ec
michael <michael2008s@gmail.com>
2019-12-06 19:57:07 +0800

    add argWorkCert comment

--- app/admin/account/model/arg_workcert.go
+++ app/admin/account/model/arg_workcert.go
@@ -3,8 +3,11 @@ package model
 import validation "github.com/go-ozzo/ozzo-validation"
 
 type ArgWorkCert struct {
+	// AccountID 账户ID
 	AccountID   int64  `json:"account_id,string" swaggertype:"string"`
+	// 审核原因
 	AuditResult string `json:"audit_result"`
+	// 通过 or 拒绝
 	Approve     bool   `json:"approve"`
 }
 
