8136c35eb129e309520c76c15a8af5e7d2607dcd
michael <michael2008s@gmail.com>
2019-12-11 17:02:48 +0800

    fix id field

--- app/service/article/dao/article.go
+++ app/service/article/dao/article.go
@@ -201,7 +201,7 @@ func (p *Dao) UpdateArticle(c context.Context, node sqalx.Node, item *model.Arti
 func (p *Dao) GetArticlesByIDs(c context.Context, node sqalx.Node, ids []int64) (items []*model.Article, err error) {
 	items = make([]*model.Article, 0)
 	strIDs := Int64Array2StringArray(ids)
-	sqlSelect := "SELECT  a.id,a.title,a.content,a.content_text,a.disable_revise,a.disable_comment,a.created_by,a.deleted,a.created_at,a.updated_at  FROM articles a WHERE a.deleted=0 AND a.di IN (?) ORDER BY a.id DESC "
+	sqlSelect := "SELECT  a.id,a.title,a.content,a.content_text,a.disable_revise,a.disable_comment,a.created_by,a.deleted,a.created_at,a.updated_at  FROM articles a WHERE a.deleted=0 AND a.id IN (?) ORDER BY a.id DESC "
 
 	if err = node.SelectContext(c, &items, sqlSelect, strings.Join(strIDs, ",")); err != nil {
 		log.For(c).Error(fmt.Sprintf("dao.GetArticlesByIDs err(%+v)", err))
