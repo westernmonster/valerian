51dc6ddf84a225dd8939aee48353fc195bdcc0b6
Jack <westernmonster@gmail.com>
2019-12-16 19:34:10 +0800

    deploy

--- app/admin/config/service/common_conf.go
+++ app/admin/config/service/common_conf.go
@@ -7,6 +7,7 @@ import (
 	"valerian/library/gid"
 )
 
+// 创建通用配置文件
 func (p *Service) CreateCommonConf(c context.Context, arg *model.ArgCreateCommonConfig) (err error) {
 	// lint config
 	if !arg.SkipLint {
@@ -29,6 +30,28 @@ func (p *Service) CreateCommonConf(c context.Context, arg *model.ArgCreateCommon
 
 	if err = p.d.AddCommonConfig(c, p.d.ConfigDB(), item); err != nil {
 		return
+	}
+
+	return
+}
+
+// 获取通用配置文件
+func (p *Service) GetCommonConf(c context.Context, id int64) (resp *model.CommonConfigResp, err error) {
+	var item *model.CommonConfig
+	if item, err = p.d.GetCommonConfigByID(c, p.d.ConfigDB(), id); err != nil {
+		return
+	}
+
+	resp = &model.CommonConfigResp{
+		ID:        item.ID,
+		TeamID:    item.TeamID,
+		Name:      item.Name,
+		Comment:   item.Comment,
+		State:     item.State,
+		Mark:      item.Mark,
+		Operator:  item.Operator,
+		CreatedAt: item.CreatedAt,
+		UpdatedAt: item.UpdatedAt,
 	}
 
 	return
