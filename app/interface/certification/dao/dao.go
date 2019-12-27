0ed2ef7670dcc3e14bb639f7379b90e887a3ad97
Jack <westernmonster@gmail.com>
2019-12-27 16:49:12 +0800

    remove logics from interface to service

--- app/interface/certification/dao/dao.go
+++ app/interface/certification/dao/dao.go
@@ -2,25 +2,16 @@ package dao
 
 import (
 	"context"
-	"fmt"
-	"time"
 
 	"valerian/app/interface/certification/conf"
 	account "valerian/app/service/account/api"
 	certification "valerian/app/service/certification/api"
-	"valerian/library/cache/memcache"
-	"valerian/library/database/sqalx"
-	"valerian/library/log"
 
 	"github.com/pkg/errors"
 )
 
 // Dao dao struct
 type Dao struct {
-	authDB           sqalx.Node
-	db               sqalx.Node
-	mc               *memcache.Pool
-	mcExpire         int32
 	c                *conf.Config
 	certificationRPC certification.CertificationClient
 	accountRPC       account.AccountClient
@@ -28,10 +19,7 @@ type Dao struct {
 
 func New(c *conf.Config) (dao *Dao) {
 	dao = &Dao{
-		c:        c,
-		db:       sqalx.NewMySQL(c.DB.Main),
-		mc:       memcache.NewPool(c.Memcache.Main.Config),
-		mcExpire: int32(time.Duration(c.Memcache.Main.Expire) / time.Second),
+		c: c,
 	}
 
 	if certificationRPC, err := certification.NewClient(c.CertificationRPC); err != nil {
@@ -48,27 +36,11 @@ func New(c *conf.Config) (dao *Dao) {
 	return
 }
 
-func (d *Dao) DB() sqalx.Node {
-	return d.db
-}
-
 // Ping check db and mc health.
 func (d *Dao) Ping(c context.Context) (err error) {
-	if err = d.db.Ping(c); err != nil {
-		log.Info(fmt.Sprintf("dao.db.Ping() error(%v)", err))
-	}
-	if err = d.pingMC(c); err != nil {
-		log.Info(fmt.Sprintf("dao.mc.Ping() error(%v)", err))
-	}
 	return
 }
 
 // Close close connections of mc, redis, db.
 func (d *Dao) Close() {
-	if d.mc != nil {
-		d.mc.Close()
-	}
-	if d.db != nil {
-		d.db.Close()
-	}
 }
