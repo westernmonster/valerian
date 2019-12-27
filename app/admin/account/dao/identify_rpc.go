82f561c724c952a3752a3817c65a8c942a58bdc6
Jack <westernmonster@gmail.com>
2019-12-27 16:02:07 +0800

    deactive

--- app/admin/account/dao/identify_rpc.go
+++ app/admin/account/dao/identify_rpc.go
@@ -7,15 +7,15 @@ import (
 	"valerian/library/log"
 )
 
-func (p *Dao) SetAccountLock(c context.Context, aid int64) (info *identify.EmptyStruct, err error) {
-	if info, err = p.identifyRPC.AccountLock(c, &identify.AidReq{Aid: aid}); err != nil {
+func (p *Dao) SetAccountLock(c context.Context, aid, targetAid int64) (info *identify.EmptyStruct, err error) {
+	if info, err = p.identifyRPC.AccountLock(c, &identify.LockReq{Aid: aid, TargetAccountID: targetAid}); err != nil {
 		log.For(c).Error(fmt.Sprintf("dao.SetAccountLock err(%+v)", err))
 	}
 	return
 }
 
-func (p *Dao) SetAccountUnlock(c context.Context, aid int64) (info *identify.EmptyStruct, err error) {
-	if info, err = p.identifyRPC.AccountUnlock(c, &identify.AidReq{Aid: aid}); err != nil {
+func (p *Dao) SetAccountUnlock(c context.Context, aid, targetAid int64) (info *identify.EmptyStruct, err error) {
+	if info, err = p.identifyRPC.AccountUnlock(c, &identify.LockReq{Aid: aid, TargetAccountID: targetAid}); err != nil {
 		log.For(c).Error(fmt.Sprintf("dao.SetAccountUnlock err(%+v)", err))
 	}
 	return
