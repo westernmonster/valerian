425a497f1a0101aa2cc14f2a144c322b0013e5c8
Jack <westernmonster@gmail.com>
2020-01-03 15:35:28 +0800

    update

--- app/interface/search/dao/es_suggest.go
+++ app/interface/search/dao/es_suggest.go
@@ -9,19 +9,13 @@ import (
 )
 
 func (d *Dao) ArticleSuggest(c context.Context, text string, size int) (res []string, err error) {
-	titleSuggester := elastic.NewCompletionSuggester("title-suggester").
-		Field("suggest_title").
+	titleSuggester := elastic.NewCompletionSuggester("suggester").
+		Field("suggest").
 		Fuzziness(2).
 		Text(text).
 		Size(size).SkipDuplicates(true)
 
-	contentSuggester := elastic.NewCompletionSuggester("content-suggester").
-		Field("suggest_content_text").
-		Fuzziness(2).
-		Text(text).
-		Size(size).SkipDuplicates(true)
-
-	searchSource := elastic.NewSearchSource().Suggester(titleSuggester).Suggester(contentSuggester).FetchSource(false).TrackScores(true)
+	searchSource := elastic.NewSearchSource().Suggester(titleSuggester).FetchSource(false).TrackScores(true)
 
 	indexName := fmt.Sprintf("%s_articles", env.DeployEnv)
 	searchResult, err := d.esClient.Search().Index(indexName).Type("article").SearchSource(searchSource).Do(c)
@@ -31,16 +25,8 @@ func (d *Dao) ArticleSuggest(c context.Context, text string, size int) (res []st
 	}
 
 	res = make([]string, 0)
-	titleResult := searchResult.Suggest["title-suggester"]
+	titleResult := searchResult.Suggest["suggester"]
 	for _, options := range titleResult {
-		for _, option := range options.Options {
-			fmt.Printf("%v ", option.Text)
-			res = append(res, option.Text)
-		}
-	}
-
-	introResult := searchResult.Suggest["content-suggester"]
-	for _, options := range introResult {
 		for _, option := range options.Options {
 			fmt.Printf("%v ", option.Text)
 			res = append(res, option.Text)
@@ -51,19 +37,13 @@ func (d *Dao) ArticleSuggest(c context.Context, text string, size int) (res []st
 }
 
 func (d *Dao) TopicSuggest(c context.Context, text string, size int) (res []string, err error) {
-	nameSuggester := elastic.NewCompletionSuggester("name-suggester").
-		Field("suggest_name").
+	nameSuggester := elastic.NewCompletionSuggester("suggester").
+		Field("suggest").
 		Fuzziness(2).
 		Text(text).
 		Size(size).SkipDuplicates(true)
 
-	introductionSuggester := elastic.NewCompletionSuggester("introduction-suggester").
-		Field("suggest_introduction").
-		Fuzziness(2).
-		Text(text).
-		Size(size).SkipDuplicates(true)
-
-	searchSource := elastic.NewSearchSource().Suggester(nameSuggester).Suggester(introductionSuggester).FetchSource(false).TrackScores(true)
+	searchSource := elastic.NewSearchSource().Suggester(nameSuggester).FetchSource(false).TrackScores(true)
 
 	indexName := fmt.Sprintf("%s_topics", env.DeployEnv)
 	searchResult, err := d.esClient.Search().Index(indexName).Type("topic").SearchSource(searchSource).Do(c)
@@ -73,16 +53,8 @@ func (d *Dao) TopicSuggest(c context.Context, text string, size int) (res []stri
 	}
 
 	res = make([]string, 0)
-	nameResult := searchResult.Suggest["name-suggester"]
+	nameResult := searchResult.Suggest["suggester"]
 	for _, options := range nameResult {
-		for _, option := range options.Options {
-			fmt.Printf("%v ", option.Text)
-			res = append(res, option.Text)
-		}
-	}
-
-	introResult := searchResult.Suggest["introduction-suggester"]
-	for _, options := range introResult {
 		for _, option := range options.Options {
 			fmt.Printf("%v ", option.Text)
 			res = append(res, option.Text)
@@ -93,19 +65,13 @@ func (d *Dao) TopicSuggest(c context.Context, text string, size int) (res []stri
 }
 
 func (d *Dao) AccountSuggest(c context.Context, text string, size int) (res []string, err error) {
-	nameSuggester := elastic.NewCompletionSuggester("name-suggester").
-		Field("suggest_user_name").
+	nameSuggester := elastic.NewCompletionSuggester("suggester").
+		Field("suggest").
 		Fuzziness(2).
 		Text(text).
 		Size(size).SkipDuplicates(true)
 
-	introductionSuggester := elastic.NewCompletionSuggester("introduction-suggester").
-		Field("suggest_introduction").
-		Fuzziness(2).
-		Text(text).
-		Size(size).SkipDuplicates(true)
-
-	searchSource := elastic.NewSearchSource().Suggester(nameSuggester).Suggester(introductionSuggester).FetchSource(false).TrackScores(true)
+	searchSource := elastic.NewSearchSource().Suggester(nameSuggester).FetchSource(false).TrackScores(true)
 
 	indexName := fmt.Sprintf("%s_accounts", env.DeployEnv)
 	searchResult, err := d.esClient.Search().Index(indexName).Type("account").SearchSource(searchSource).Do(c)
@@ -115,16 +81,8 @@ func (d *Dao) AccountSuggest(c context.Context, text string, size int) (res []st
 	}
 
 	res = make([]string, 0)
-	nameResult := searchResult.Suggest["name-suggester"]
+	nameResult := searchResult.Suggest["suggester"]
 	for _, options := range nameResult {
-		for _, option := range options.Options {
-			fmt.Printf("%v ", option.Text)
-			res = append(res, option.Text)
-		}
-	}
-
-	introResult := searchResult.Suggest["introduction-suggester"]
-	for _, options := range introResult {
 		for _, option := range options.Options {
 			fmt.Printf("%v ", option.Text)
 			res = append(res, option.Text)
@@ -135,19 +93,13 @@ func (d *Dao) AccountSuggest(c context.Context, text string, size int) (res []st
 }
 
 func (d *Dao) DiscussionSuggest(c context.Context, text string, size int) (res []string, err error) {
-	nameSuggester := elastic.NewCompletionSuggester("name-suggester").
-		Field("suggest_title").
+	nameSuggester := elastic.NewCompletionSuggester("suggester").
+		Field("suggest").
 		Fuzziness(2).
 		Text(text).
 		Size(size).SkipDuplicates(true)
 
-	introductionSuggester := elastic.NewCompletionSuggester("content-suggester").
-		Field("suggest_content_text").
-		Fuzziness(2).
-		Text(text).
-		Size(size).SkipDuplicates(true)
-
-	searchSource := elastic.NewSearchSource().Suggester(nameSuggester).Suggester(introductionSuggester).FetchSource(false).TrackScores(true)
+	searchSource := elastic.NewSearchSource().Suggester(nameSuggester).FetchSource(false).TrackScores(true)
 
 	indexName := fmt.Sprintf("%s_discussions", env.DeployEnv)
 	searchResult, err := d.esClient.Search().Index(indexName).Type("discussion").SearchSource(searchSource).Do(c)
@@ -157,16 +109,8 @@ func (d *Dao) DiscussionSuggest(c context.Context, text string, size int) (res [
 	}
 
 	res = make([]string, 0)
-	nameResult := searchResult.Suggest["name-suggester"]
+	nameResult := searchResult.Suggest["suggester"]
 	for _, options := range nameResult {
-		for _, option := range options.Options {
-			fmt.Printf("%v ", option.Text)
-			res = append(res, option.Text)
-		}
-	}
-
-	introResult := searchResult.Suggest["content-suggester"]
-	for _, options := range introResult {
 		for _, option := range options.Options {
 			fmt.Printf("%v ", option.Text)
 			res = append(res, option.Text)
