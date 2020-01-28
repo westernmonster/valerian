f8a77d1571c1e75750b853c35d469c280ae557c0
Jack <westernmonster@gmail.com>
2020-01-29 02:22:08 +0800

    增加国际化错误码

--- app/interface/feedback/dao/feedback.go
+++ app/interface/feedback/dao/feedback.go
@@ -9,6 +9,19 @@ import (
 	"valerian/library/log"
 )
 
+// GetAll get all records
+func (p *Dao) GetFeedbacks(c context.Context, node sqalx.Node) (items []*model.Feedback, err error) {
+	items = make([]*model.Feedback, 0)
+	sqlSelect := "SELECT a.id,a.target_id,a.target_type,a.target_desc,a.feedback_type,a.feedback_desc,a.created_by,a.deleted,a.created_at,a.updated_at,a.verify_status,a.verify_desc FROM feedbacks a WHERE a.deleted=0 ORDER BY a.id DESC "
+
+	if err = node.SelectContext(c, &items, sqlSelect); err != nil {
+		log.For(c).Error(fmt.Sprintf("dao.GetFeedbacks err(%+v)", err))
+		return
+	}
+	return
+}
+
+// GetAllByCondition get records by condition
 func (p *Dao) GetFeedbacksByCond(c context.Context, node sqalx.Node, cond map[string]interface{}) (items []*model.Feedback, err error) {
 	items = make([]*model.Feedback, 0)
 	condition := make([]interface{}, 0)
@@ -28,10 +41,6 @@ func (p *Dao) GetFeedbacksByCond(c context.Context, node sqalx.Node, cond map[st
 	}
 	if val, ok := cond["target_desc"]; ok {
 		clause += " AND a.target_desc =?"
-		condition = append(condition, val)
-	}
-	if val, ok := cond["target_user_id"]; ok {
-		clause += " AND a.target_user_id =?"
 		condition = append(condition, val)
 	}
 	if val, ok := cond["feedback_type"]; ok {
@@ -55,7 +64,7 @@ func (p *Dao) GetFeedbacksByCond(c context.Context, node sqalx.Node, cond map[st
 		condition = append(condition, val)
 	}
 
-	sqlSelect := fmt.Sprintf("SELECT a.id,a.target_id,a.target_type,a.target_desc,a.target_user_id,a.feedback_type,a.feedback_desc,a.created_by,a.deleted,a.created_at,a.updated_at,a.verify_status,a.verify_desc FROM feedbacks a WHERE a.deleted=0 %s ORDER BY a.id DESC", clause)
+	sqlSelect := fmt.Sprintf("SELECT a.id,a.target_id,a.target_type,a.target_desc,a.feedback_type,a.feedback_desc,a.created_by,a.deleted,a.created_at,a.updated_at,a.verify_status,a.verify_desc FROM feedbacks a WHERE a.deleted=0 %s ORDER BY a.id DESC", clause)
 
 	if err = node.SelectContext(c, &items, sqlSelect, condition...); err != nil {
 		log.For(c).Error(fmt.Sprintf("dao.GetFeedbacksByCond err(%+v), condition(%+v)", err, cond))
@@ -67,7 +76,7 @@ func (p *Dao) GetFeedbacksByCond(c context.Context, node sqalx.Node, cond map[st
 // GetByID get a record by ID
 func (p *Dao) GetFeedbackByID(c context.Context, node sqalx.Node, id int64) (item *model.Feedback, err error) {
 	item = new(model.Feedback)
-	sqlSelect := "SELECT a.id,a.target_id,a.target_type,a.target_desc,a.target_user_id,a.feedback_type,a.feedback_desc,a.created_by,a.deleted,a.created_at,a.updated_at,a.verify_status,a.verify_desc FROM feedbacks a WHERE a.id=? AND a.deleted=0"
+	sqlSelect := "SELECT a.id,a.target_id,a.target_type,a.target_desc,a.feedback_type,a.feedback_desc,a.created_by,a.deleted,a.created_at,a.updated_at,a.verify_status,a.verify_desc FROM feedbacks a WHERE a.id=? AND a.deleted=0"
 
 	if err = node.GetContext(c, item, sqlSelect, id); err != nil {
 		if err == sql.ErrNoRows {
@@ -103,10 +112,6 @@ func (p *Dao) GetFeedbackByCond(c context.Context, node sqalx.Node, cond map[str
 		clause += " AND a.target_desc =?"
 		condition = append(condition, val)
 	}
-	if val, ok := cond["target_user_id"]; ok {
-		clause += " AND a.target_user_id =?"
-		condition = append(condition, val)
-	}
 	if val, ok := cond["feedback_type"]; ok {
 		clause += " AND a.feedback_type =?"
 		condition = append(condition, val)
@@ -128,7 +133,7 @@ func (p *Dao) GetFeedbackByCond(c context.Context, node sqalx.Node, cond map[str
 		condition = append(condition, val)
 	}
 
-	sqlSelect := fmt.Sprintf("SELECT a.id,a.target_id,a.target_type,a.target_desc,a.target_user_id,a.feedback_type,a.feedback_desc,a.created_by,a.deleted,a.created_at,a.updated_at,a.verify_status,a.verify_desc FROM feedbacks a WHERE a.deleted=0 %s", clause)
+	sqlSelect := fmt.Sprintf("SELECT a.id,a.target_id,a.target_type,a.target_desc,a.feedback_type,a.feedback_desc,a.created_by,a.deleted,a.created_at,a.updated_at,a.verify_status,a.verify_desc FROM feedbacks a WHERE a.deleted=0 %s", clause)
 
 	if err = node.GetContext(c, item, sqlSelect, condition...); err != nil {
 		if err == sql.ErrNoRows {
@@ -145,9 +150,9 @@ func (p *Dao) GetFeedbackByCond(c context.Context, node sqalx.Node, cond map[str
 
 // Insert insert a new record
 func (p *Dao) AddFeedback(c context.Context, node sqalx.Node, item *model.Feedback) (err error) {
-	sqlInsert := "INSERT INTO feedbacks( id,target_id,target_type,target_desc,target_user_id,feedback_type,feedback_desc,created_by,deleted,created_at,updated_at,verify_status,verify_desc) VALUES ( ?,?,?,?,?,?,?,?,?,?,?,?,?)"
+	sqlInsert := "INSERT INTO feedbacks( id,target_id,target_type,target_desc,feedback_type,feedback_desc,created_by,deleted,created_at,updated_at,verify_status,verify_desc) VALUES ( ?,?,?,?,?,?,?,?,?,?,?,?)"
 
-	if _, err = node.ExecContext(c, sqlInsert, item.ID, item.TargetID, item.TargetType, item.TargetDesc, item.TargetUserID, item.FeedbackType, item.FeedbackDesc, item.CreatedBy, item.Deleted, item.CreatedAt, item.UpdatedAt, item.VerifyStatus, item.VerifyDesc); err != nil {
+	if _, err = node.ExecContext(c, sqlInsert, item.ID, item.TargetID, item.TargetType, item.TargetDesc, item.FeedbackType, item.FeedbackDesc, item.CreatedBy, item.Deleted, item.CreatedAt, item.UpdatedAt, item.VerifyStatus, item.VerifyDesc); err != nil {
 		log.For(c).Error(fmt.Sprintf("dao.AddFeedbacks err(%+v), item(%+v)", err, item))
 		return
 	}
@@ -157,9 +162,9 @@ func (p *Dao) AddFeedback(c context.Context, node sqalx.Node, item *model.Feedba
 
 // Update update a exist record
 func (p *Dao) UpdateFeedback(c context.Context, node sqalx.Node, item *model.Feedback) (err error) {
-	sqlUpdate := "UPDATE feedbacks SET target_id=?,target_type=?,target_desc=?,target_user_id=?,feedback_type=?,feedback_desc=?,created_by=?,updated_at=?,verify_status=?,verify_desc=? WHERE id=?"
+	sqlUpdate := "UPDATE feedbacks SET target_id=?,target_type=?,target_desc=?,feedback_type=?,feedback_desc=?,created_by=?,updated_at=?,verify_status=?,verify_desc=? WHERE id=?"
 
-	_, err = node.ExecContext(c, sqlUpdate, item.TargetID, item.TargetType, item.TargetDesc, item.TargetUserID, item.FeedbackType, item.FeedbackDesc, item.CreatedBy, item.UpdatedAt, item.VerifyStatus, item.VerifyDesc, item.ID)
+	_, err = node.ExecContext(c, sqlUpdate, item.TargetID, item.TargetType, item.TargetDesc, item.FeedbackType, item.FeedbackDesc, item.CreatedBy, item.UpdatedAt, item.VerifyStatus, item.VerifyDesc, item.ID)
 	if err != nil {
 		log.For(c).Error(fmt.Sprintf("dao.UpdateFeedbacks err(%+v), item(%+v)", err, item))
 		return
